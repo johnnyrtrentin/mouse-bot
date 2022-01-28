0d1a8d2f09cd7859aeaab4c2c89639ae47babbac
Johnny Trentin <johnnyrtrentin@gmail.com>
2022-01-28 01:34:39 -0300

    feat: recording position and left click mouse

---
+++ bot-mouse-recorder/bot-mouse-recorder/MouseEvents.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace bot_mouse_recorder
+{
+    partial class MouseEvents
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Btn1
+            // 
+            this.Btn1.Location = new System.Drawing.Point(77, 12);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(111, 41);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.Text = "Record";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.Location = new System.Drawing.Point(77, 105);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(112, 41);
+            this.Btn3.TabIndex = 1;
+            this.Btn3.Text = "Play";
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.Btn3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Location = new System.Drawing.Point(77, 59);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(112, 41);
+            this.Btn2.TabIndex = 3;
+            this.Btn2.Text = "Stop";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // MouseEvents
+            // 
+            this.AcceptButton = this.Btn1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 159);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn3);
+            this.Controls.Add(this.Btn1);
+            this.Name = "MouseEvents";
+            this.Text = "MouseEvents";
+            this.Load += new System.EventHandler(this.MouseEvents_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ bot-mouse-recorder/bot-mouse-recorder/MouseEvents.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace bot_mouse_recorder
+{
+    public partial class MouseEvents : Form
+    {
+        ListViewItem lv;
+        private readonly ListView listView1 = new ListView();
+        private int a, b;
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public MouseEvents()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseEvents_Load(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            this.SetRecordButtonText("Recording");
+            timer1.Start();
+
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+            Cursor.Position = new System.Drawing.Point(x, y);
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            this.SetRecordButtonText("Record");
+            timer1.Stop();
+            timer2.Stop();
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            this.SetRecordButtonText("Record");
+            timer2.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.RecordMousePosition();
+            this.RecordMouseClicks();
+            this.IncreaseHandler();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (a != b)
+            {
+                Cursor.Position = new System.Drawing.Point(
+                    int.Parse(listView1.Items[a].SubItems[0].Text),
+                    int.Parse(listView1.Items[a].SubItems[1].Text)
+                );
+
+                if (listView1.Items[a].SubItems[2].Text == "Left")
+                {
+                    this.ReproduceMouseClick();
+                    Thread.Sleep(500);
+                }
+
+                this.IncreaseMainHandler();
+            }
+        }
+        private void RecordMousePosition()
+        {
+            lv = new ListViewItem(Cursor.Position.X.ToString());
+            lv.SubItems.Add(Cursor.Position.Y.ToString());
+            listView1.Items.Add(lv);
+
+        }
+ 
+        private void RecordMouseClicks()
+        {
+            lv.SubItems.Add(MouseButtons.ToString());
+        }
+
+        private void ReproduceMouseClick()
+        {
+            try
+            {
+                var x = Convert.ToInt32(listView1.Items[a].SubItems[0].Text);
+                var y = Convert.ToInt32(listView1.Items[a].SubItems[1].Text);
+
+                mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void SetRecordButtonText(String value)
+        {
+            Btn1.Text = value;
+        }
+
+        private void IncreaseMainHandler()
+        {
+            a++;
+        }
+
+        private void IncreaseHandler()
+        {
+            b++;
+        }
+    }
+}
